824d4876bd7b5a0170add8e7bf8d727e3c4b3ec6
青.꧔ <93132738+qing-wq@users.noreply.github.com>
2024-05-07 16:03:54 +0800

    Register webservice to kong gateway (#36)
    * feat: update webservice script
    
    * feat: register service to kong gateway
    
    * Address PR comment
    
    * fix: update kong admin url

--- hashicorp/webservice/instances/alicloud/ali-ws.tf
+++ hashicorp/webservice/instances/alicloud/ali-ws.tf
@@ -65,6 +65,26 @@ variable "internet_max_bandwidth_out" {
   default     = 1
 }
 
+variable "kong_instance_name" {
+  type        = string
+  description = "The Kong admin URI"
+}
+
+variable "service_port" {
+  type        = number
+  description = "The port of the service"
+}
+
+variable "service_name" {
+  type        = string
+  description = "The name of the service"
+}
+
+variable "gateway_route_path_list" {
+  type        = list(string)
+  description = "The path of the gateway route"
+}
+
 data "alicloud_security_groups" "ws-groups" {
   name_regex = join("|", var.security_group_names)
 }
@@ -79,6 +99,11 @@ data "template_file" "ws-init" {
 data "alicloud_images" "ws-images" {
   image_name = var.ali_image_name
   owners     = "self"
+}
+
+data "alicloud_instances" "kong-instance" {
+  instance_name = var.kong_instance_name
+  status        = "Running"
 }
 
 resource "alicloud_instance" "ws-instance" {
@@ -105,6 +130,24 @@ resource "alicloud_instance" "ws-instance" {
   user_data = data.template_file.ws-init.rendered
 }
 
+// kong service
+resource "kong_service" "ws-service" {
+  name     = var.service_name
+  protocol = "http"
+  port     = var.service_port
+}
+
+// kong route
+resource "kong_route" "ws-route" {
+  name           = "${var.service_name}_route"
+  protocols      = ["https"]
+  methods        = ["GET", "POST", "PUT", "DELETE"]
+  paths          = var.gateway_route_path_list
+  strip_path     = true
+  regex_priority = 1
+  service_id     = kong_service.ws-service.id
+}
+
 terraform {
   required_providers {
     alicloud = {
@@ -115,9 +158,17 @@ terraform {
       source  = "hashicorp/template"
       version = "2.2.0"
     }
+    kong = {
+      source  = "kevholditch/kong"
+      version = "6.5.1"
+    }
   }
 
   required_version = ">= 0.14.5"
 }
 
 provider "alicloud" {}
+
+provider "kong" {
+  kong_admin_uri = "https://${data.alicloud_instance.kong-instance.instances[0].private_ip}:8444"
+}
